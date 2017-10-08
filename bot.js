35d381ff26ace08cae714498acd8d1646c7ebbd7
Calvin Truong <uoi100x@gmail.com>
2017-10-08 07:25:23 -0700

    Implemented !kick command

--- bot.js
+++ bot.js
@@ -6,6 +6,52 @@ const bot = new Discord.Client({disableEveryone: true});
 // Standard Bot Features - Banning, Muting, Greeting, Roles
 function msgStd(message)
 {
+	let args = message.content.split(" ");
+	let cmd = args[0];
+	let prefix = botSettings.prefix;
+	
+	// Remove the command from the args
+	args = args.slice(1);
+	
+	if(cmd === `${prefix}kick`)
+	{
+		let hasKick = message.member.hasPermission("KICK_MEMBERS");
+			
+		let target = message.mentions.members.first();
+		
+		// Check if target exists
+		if(target === undefined || target != args[0]){
+			message.channel.send(`Invalid argument please use the following format:\n
+				!kick @username reason.`);
+			return;
+		}
+		
+		let reason = message.content.slice(cmd.length + args[0].length + 2);
+		
+		if(hasKick)
+		{
+			target.kick(reason)
+				.then(member => 
+				{
+					let embed = new Discord.RichEmbed()
+						.setTitle("Kicked")
+						.setAuthor(target.user.username, target.user.avatarURL)
+						.setColor(0xFF0000)
+						.setTimestamp()
+						.setDescription(reason);
+					
+					message.channel.send({embed});
+				})
+				.catch(member =>
+				{
+					message.channel.send(`Unable to kick the user. Maybe they're above me in the hierarchy? :thinking:`)
+				});
+		}
+		else
+		{
+			message.reply("You don't have the KICK_MEMBERS permission to kick someone.");
+		}
+	}
 
 }
 
